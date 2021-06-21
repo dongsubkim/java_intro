a323671b8f58323ffd592b33ce11c2108766ef56
DongSubKim <dskim9218@gmail.com>
2021-06-21 23:53:26 +0900

    part 6 overloading constructor

--- Part_6/Car.java
+++ Part_6/Car.java
@@ -7,4 +7,15 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
+
+    public Car() {
+        // this.name = "no name";
+        // this.number = 0;
+        this("no name", 0);
+    }
+
+    public Car(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
 }
--- Part_6/CarExam.java
+++ Part_6/CarExam.java
@@ -2,9 +2,9 @@ package Part_6;
 
 public class CarExam {
     public static void main(String[] args) {
-        // Car c1 = new Car();
+        Car c1 = new Car();
         Car c2 = new Car("firetruck");
-        Car c3 = new Car("ambulance");
+        Car c3 = new Car("ambulance", 1111);
 
         System.out.println(c2.name);
     }
