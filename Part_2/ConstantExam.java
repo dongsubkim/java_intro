da1d9048a577d59bc47ea13d4127d0c7b37d2a28
DongSubKim <dskim9218@gmail.com>
2021-06-21 19:23:50 +0900

    part 2 constant

---
+++ Part_2/ConstantExam.java
@@ -0,0 +1,23 @@
+package Part_2;
+
+public class ConstantExam {
+    public static void main(String[] args) {
+        int i;
+        i = 10;
+        i = 5;
+
+        final int J; // constant : All caps
+        J = 10;
+
+        // J = 5;
+
+        double circleArea;
+        final double PI = 3.14159;
+        circleArea = 3 * 3 * PI;
+
+        final int OIL_PRICE = 1390; // constant: Underbar between words
+
+        int totalPrice = 50 * OIL_PRICE;
+
+    }
+}
