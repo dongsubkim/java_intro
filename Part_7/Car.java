3187baf5b06d4b0e91bc6306f308981dc547b583
DongSubKim <dskim9218@gmail.com>
2021-06-22 01:05:06 +0900

    part 7 super

--- Part_7/Car.java
+++ Part_7/Car.java
@@ -4,4 +4,8 @@ public class Car {
     public void run() {
         System.out.println("run");
     }
+
+    public Car(String name) {
+        System.out.println("Car constructor");
+    }
 }
---
+++ Part_7/Truck.java
@@ -0,0 +1,8 @@
+package Part_7;
+
+public class Truck extends Car {
+    public Truck() {
+        super("Firetruck");
+        System.out.println("truck constructor");
+    }
+}
