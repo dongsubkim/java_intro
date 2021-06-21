59c627d3278753d661c8bcce985d15c2b12e1338
DongSubKim <dskim9218@gmail.com>
2021-06-21 21:14:10 +0900

    part 3 for

---
+++ Part_3/ForExam.java
@@ -0,0 +1,17 @@
+package Part_3;
+
+public class ForExam {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 100; i++) {
+            // if (i % 2 != 0) {
+            // continue;
+            // }
+            total += i;
+            if (i == 50) {
+                break;
+            }
+        }
+        System.out.println(total);
+    }
+}
