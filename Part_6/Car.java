b6749e2d5e7f2229e0c62cb2cf11b4370583c473
DongSubKim <dskim9218@gmail.com>
2021-06-21 23:33:53 +0900

    part 6 constructor

---
+++ Part_6/Car.java
@@ -0,0 +1,10 @@
+package Part_6;
+
+public class Car {
+    String name;
+    int number;
+
+    public Car(String n) {
+        name = n;
+    }
+}
---
+++ Part_6/CarExam.java
@@ -0,0 +1,11 @@
+package Part_6;
+
+public class CarExam {
+    public static void main(String[] args) {
+        // Car c1 = new Car();
+        Car c2 = new Car("firetruck");
+        Car c3 = new Car("ambulance");
+
+        System.out.println(c2.name);
+    }
+}
