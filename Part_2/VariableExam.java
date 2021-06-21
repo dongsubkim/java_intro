30e97429f3856f9dc1bbc4ebcaaa23a67436193c
DongSubKim <dskim9218@gmail.com>
2021-06-21 19:20:24 +0900

    part 2 variable exam

---
+++ Part_2/VariableExam.java
@@ -0,0 +1,21 @@
+package Part_2;
+
+public class VariableExam {
+    public static void main(String[] args) {
+        int count;
+
+        count = 10;
+
+        count = 20;
+
+        System.out.println(count);
+
+        // count = 11.1;
+
+        double avg = 11.1;
+
+        String name = "exercise";
+
+        int totalCount; // camelCase
+    }
+}
