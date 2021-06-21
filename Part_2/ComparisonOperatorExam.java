37598cf097e93900104e0ca53c6191ec46cfd9c0
DongSubKim <dskim9218@gmail.com>
2021-06-21 20:03:41 +0900

    part 2 comparison operator

---
+++ Part_2/ComparisonOperatorExam.java
@@ -0,0 +1,17 @@
+package Part_2;
+
+public class ComparisonOperatorExam {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 10;
+        System.out.println(i == j);
+        System.out.println(i != j);
+        System.out.println(i < j);
+        System.out.println(i <= j);
+        System.out.println(i > j);
+        System.out.println(i >= j);
+
+        i += 10; // i = i + 10;
+        System.out.println(i);
+    }
+}
