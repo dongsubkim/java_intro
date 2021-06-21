fc591a333289359c371e27c0a0f4e71017a9c2c5
DongSubKim <dskim9218@gmail.com>
2021-06-21 19:04:16 +0900

    part 1

---
+++ Part_1/HelloWorld.java
@@ -0,0 +1,17 @@
+package Part_1;
+
+public class HelloWorld {
+    /**
+     * docstring
+     * 
+     * @param args
+     */
+    // program starts.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // System.out.println("1");
+        /*
+         * System.out.println("2"); System.out.println("3"); System.out.println("4");
+         */
+    }
+}
