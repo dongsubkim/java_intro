e92953e92e6c9433457623efda5b6e5ea3bf36a4
DongSubKim <dskim9218@gmail.com>
2021-06-21 19:43:22 +0900

    part 2 primitive data type

---
+++ Part_2/PrimitiveDataTypeExam.java
@@ -0,0 +1,19 @@
+package Part_2;
+
+public class PrimitiveDataTypeExam {
+    public static void main(String[] args) {
+        boolean isFun = true;
+        System.out.println(isFun);
+
+        char c = 'f';
+
+        int x = 59;
+
+        long big = 34353252453L;
+
+        float f = 32.4f;
+
+        double d = 2345224.24636;
+
+    }
+}
